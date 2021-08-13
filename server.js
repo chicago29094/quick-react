41ad53b3e1e61754c13b69097eb3ff2ddd8b8f32
chicago29094 <harry.anastopoulos@telusys.net>
2021-08-12 19:34:03 -0500

    No Changes

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express();
 const cors = require('cors');
 const { NaryNode, NaryTree } = require('./utility/NaryTree');
 
+console.log(process);
+console.log(process.env);
+console.log(process.env.PORT);
+
 const PORT = process.env.PORT || 4000;
+
 
 const jsonOnlyErrorHandler = async (err, req, res, next) => {
     res.status(500).send({ "ErrorMessage": "An invalid request was submitted.  Please check your input and try again.", "error": err });
@@ -21,6 +26,8 @@ app.use('/api', userController);
 // Project model controller and routes
 const projectController = require('./controllers/project');
 app.use('/project', projectController);
+
+console.log('Here:00005');
 
 app.listen(PORT, () => {
   console.log('Listening on port:', PORT)
