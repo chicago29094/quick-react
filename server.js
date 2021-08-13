f5abcb97fc0d1ccb6d876ad272c371b6eeac1692
chicago29094 <harry.anastopoulos@telusys.net>
2021-08-12 19:44:31 -0500

    Forgot to delete an import statement

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const app = express();
 const cors = require('cors');
 const { NaryNode, NaryTree } = require('./utility/NaryTree');
 
-console.log(process);
-console.log(process.env);
-console.log(process.env.PORT);
-
 const PORT = process.env.PORT || 4000;
-
 
 const jsonOnlyErrorHandler = async (err, req, res, next) => {
     res.status(500).send({ "ErrorMessage": "An invalid request was submitted.  Please check your input and try again.", "error": err });
@@ -26,8 +21,6 @@ app.use('/api', userController);
 // Project model controller and routes
 const projectController = require('./controllers/project');
 app.use('/project', projectController);
-
-console.log('Here:00005');
 
 app.listen(PORT, () => {
   console.log('Listening on port:', PORT)
